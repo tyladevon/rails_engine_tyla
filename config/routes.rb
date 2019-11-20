28567047ce0d8bd5283d49ccc4298007b938cc0d
Tyla Devon <tyladevon@gmail.com>
2019-11-20 08:53:30 -0700

    Add routes, serializer, and controller for merchants index and show.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      get '/merchants', to: "merchants#index"
+      get '/merchants/:id', to: "merchants#show"
+      # get '/merchants/find?params', to: "merchants#find"
+    end
+  end
 end
