b624fbcad2fb3d381ad8b2c78a613dec3fa74c8f
Tyla Devon <tyladevon@gmail.com>
2019-11-21 11:09:38 -0700

    Create outlines for testing resource endpoints.

---
+++ spec/requests/api/v1/transaction_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it 'sends a list of merchants' do
+      create_list(:merchant, 3)
+
+      get '/api/v1/merchants'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body)
+
+      expect(merchants.count).to eq(3)
+  end
+
+  it "can get one item by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["data"]["id"]).to eq(id.to_s)
+  end
+
+  it "can find by attributes" do
+    merchant_attributes = attributes_for(:merchant)
+    merchant = create(:merchant, merchant_attributes)
+
+    get "/api/v1/merchants/find?name=#{merchant.name}"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(response_body[:data][:attributes][:name]).to eq(merchant.name)
+
+    get "/api/v1/merchants/find?created_at=#{merchant.created_at}"
+
+    expect(response).to be_successful
+    expect(response_body[:data][:attributes][:name]).to eq(merchant.name)
+
+  end
+end
