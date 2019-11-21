ba88918c4fcb1e6838bb660a6610c8b9db367121
Tyla Devon <tyladevon@gmail.com>
2019-11-21 10:59:27 -0700

    Refactor merchants controller to include find and find_all actions.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -10,4 +10,22 @@ class Api::V1::MerchantsController < ApplicationController
     render json: serialized_merchant
   end
 
+  def find
+   merchant = Merchant.find_by(find_params)
+   serialized_merchant = MerchantSerializer.new(merchant)
+   render json: serialized_merchant
+  end
+
+  def find_all
+      merchant = Merchant.where(find_params)
+      serialized_merchant = MerchantSerializer.new(merchant)
+      render json: serialized_merchant
+  end
+
+  private
+
+  def find_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
+
 end
